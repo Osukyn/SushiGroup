26224b67700faa7bc0dfff8db104a6ae29e56fa0
Osukyn <osukyn@gmail.com>
2023-09-27 14:12:53 +0200

    ✨ feat: Add GroupOrder class and methods
    - Added GroupOrder class with properties host, users, and status
    - Implemented addUser method to add a user to the group order
    - Implemented removeUser method to remove a user from the group order
    - Implemented getUserOrder method to get the order of a specific user
    - Implemented setUserOrder method to set the order of a specific user
    - Implemented getHostOrder method to get the host's order
    - Implemented setHostOrder method to set the host's order
    - Implemented getHost method to get the host of the group order
    - Implemented getUsers method to get all users in the group order
    - Implemented getStatus method to get the status of the group order
    - Implemented setStatus method to set the status of the group order
    - Implemented getItems method to retrieve all items in the group order

--- server/src/models/Order.ts
+++ server/src/models/Order.ts
@@ -1,25 +1,109 @@
+import {User} from "./User";
+
 export class Order {
-    items: OrderItem[];
-    email: string;
-    date: Date;
-    status: OrderStatus = OrderStatus.EN_COURS;
-    constructor(email: string) {
-        this.items = [];
-        this.email = email;
-        this.date = new Date();
-    }
+  items: OrderItem[];
+  email: string;
+  date: Date;
+  status: OrderStatus = OrderStatus.EN_COURS;
+
+  constructor(email: string) {
+    this.items = [];
+    this.email = email;
+    this.date = new Date();
+  }
 }
 
 export enum OrderStatus {
-    EN_COURS = 'En cours',
-    CONFIRMED = 'Confirmée',
+  EN_COURS = 'En cours',
+  CONFIRMED = 'Confirmée',
 }
 
 export class OrderItem {
-    code: string;
-    qte: number;
-    constructor(code: string, qte: number) {
-        this.code = code;
-        this.qte = qte;
+  code: string;
+  qte: number;
+
+  constructor(code: string, qte: number) {
+    this.code = code;
+    this.qte = qte;
+  }
+}
+
+export class GroupOrder {
+  host: User;
+  users: User[];
+  status: OrderStatus = OrderStatus.EN_COURS;
+
+  constructor(host: User) {
+    this.host = host;
+    this.users = [];
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
+  }
+
+  removeUser(user: User) {
+    const index = this.users.findIndex(u => u.email === user.email);
+    if (index !== -1) {
+      this.users.splice(index, 1);
     }
+  }
+
+  getUserOrder(user: User): Order | undefined {
+    const index = this.users.findIndex(u => u.email === user.email);
+    if (index !== -1) {
+      return this.users[index].order;
+    }
+  }
+
+  setUserOrder(user: User, order: Order) {
+    const index = this.users.findIndex(u => u.email === user.email);
+    if (index !== -1) {
+      this.users[index].order = order;
+    }
+  }
+
+  getHostOrder(): Order | undefined {
+    return this.host.order;
+  }
+
+  setHostOrder(order: Order) {
+    this.host.order = order;
+  }
+
+  getHost(): User {
+    return this.host;
+  }
+
+  getUsers(): User[] {
+    return this.users;
+  }
+
+  getStatus(): OrderStatus {
+    return this.status;
+  }
+
+  setStatus(status: OrderStatus) {
+    this.status = status;
+  }
+
+  getItems(): OrderItem[] {
+    let items: OrderItem[] = [];
+    if (this.host.order) {
+      items = this.host.order.items;
+    }
+    this.users.forEach(user => {
+      if (user.order) {
+        user.order.items.forEach(item => {
+          const index = items.findIndex(i => i.code === item.code);
+          if (index !== -1) {
+            items[index].qte += item.qte;
+          } else {
+            items.push(item);
+          }
+        });
+      }
+    });
+    return items;
+  }
 }
