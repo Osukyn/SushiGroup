887c48c3a733458b99ae41901e3786cb488019ba
Osukyn <osukyn@gmail.com>
2023-10-02 02:49:48 +0200

    :sparkles: Remove create-group component and related files
    - Removed create-group.component.ts, create-group.component.html, create-group.component.css
    - Removed import statements for CreateGroupComponent in app-routing.module.ts and app.module.ts

--- src/app/group-choice/group-choice.component.ts
+++ src/app/group-choice/group-choice.component.ts
@@ -4,6 +4,10 @@ import {Group} from "../model/group.model";
 import {BehaviorSubject, Observable, Subscription} from "rxjs";
 import {TuiDialogService} from "@taiga-ui/core";
 import {OrderStatus} from "../model/order.model";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {OrderService} from "../order.service";
+import {UserService} from "../user.service";
+import {TuiStringHandler} from "@taiga-ui/cdk";
 
 @Component({
   selector: 'app-group-choice',
@@ -16,14 +20,24 @@ export class GroupChoiceComponent implements OnInit, OnDestroy {
   private subscription: Subscription | undefined;
   public loaded = false;
   private _groups$ = new BehaviorSubject<Group[]>([]);
+  open = false;
+  restoForm: FormControl = new FormControl(null, Validators.required);
+  form = new FormGroup({
+    resto: this.restoForm
+  });
+  restaurantsList: any[] = [];
 
-  constructor(public socketService: SocketService) {}
+  constructor(public socketService: SocketService, private orderService: OrderService, public userService: UserService) {}
 
   ngOnInit(): void {
     this.socketService.getGroups();
+    this.orderService.getRestaurantAddress() ? this.restoForm.setValue(this.orderService.getRestaurantAddress()) : this.restoForm.setValue('');
     this.subscription = this.socketService.groupsUpdate.subscribe(groups => {
       this.groups = groups;
       this._groups$.next(this.groups);
+    });
+    this.orderService.getRestaurantList().subscribe(restaurants => {
+      this.restaurantsList = restaurants;
     });
   }
 
@@ -36,4 +50,27 @@ export class GroupChoiceComponent implements OnInit, OnDestroy {
   }
 
   protected readonly OrderStatus = OrderStatus;
+
+  stringify: TuiStringHandler<any> = restaurant => restaurant.name;
+
+  showDialog(): void {
+    this.open = true;
+  }
+
+  createGroup(observer: any): void {
+    if (this.restoForm.valid) {
+      this.socketService.createGroup(this.restoForm.value);
+      observer.complete();
+    }
+  }
+
+  libelleByRestaurantId(restaurantId: number): string {
+    const resto = this.restaurantsList.find(r => r.id === restaurantId);
+    return resto ? resto.libelle : '';
+  }
+
+  closeDialog(observer: any): void {
+    observer.complete();
+  }
+
 }
