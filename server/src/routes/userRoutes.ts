3623b2a41c21b4aa27b1299517cb630a4a693a04
Osukyn <osukyn@gmail.com>
2023-10-09 02:13:04 +0200

    ✨ Add updateUser endpoint
    - Added updateUser function to userController.ts
    - Added PUT /user route to userRoutes.ts

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post('/register', userController.register);
 router.get('/user', userController.getUser);
+router.put('/user', userController.updateUser);
 router.get('/isUserRegistered', userController.isUserRegistered);
 router.get('/isUserInGroup', userController.isUserInGroup);
 router.get('/getUserGroup', userController.getUserGroup);
