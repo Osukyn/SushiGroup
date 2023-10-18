9db0d54211438a8942aae3073d8a6e0bb6b76eae
Osukyn <osukyn@gmail.com>
2023-10-18 13:50:54 +0200

    ⚡️ Upgrade update process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -116,9 +116,7 @@ import { OrderHistoryComponent } from './order-history/order-history.component';
         TuiBlockStatusModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
             enabled: !isDevMode(),
-            // Register the ServiceWorker as soon as the application is stable
-            // or after 30 seconds (whichever comes first).
-            registrationStrategy: 'registerWhenStable:30000'
+            registrationStrategy: 'registerImmediately'
         }),
         TuiInputPhoneModule,
         TuiHintModule,
