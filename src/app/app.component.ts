78bc2921c5cb92675679a97c3b6e496e94d3dbff
Osukyn <osukyn@gmail.com>
2023-10-18 15:01:57 +0200

    ✨ Add check for update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -72,6 +72,7 @@ export class AppComponent implements OnInit {
             }
         });
         this.updateClient();
+      if (this.update.isEnabled) setInterval(() => this.update.checkForUpdate().then(() => console.log('Checking for update')), 1000 * 5);
     }
 
     ngOnInit() {
