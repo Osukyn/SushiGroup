6451904c49138b89a14f2f1b384850a47b2a6bbf
Osukyn <osukyn@gmail.com>
2023-11-28 13:53:59 +0100

    ✨ Add get last order route and logic

--- server/src/routes/orderRoutes.ts
+++ server/src/routes/orderRoutes.ts
@@ -4,5 +4,6 @@ import * as orderController from "../controllers/orderController";
 const router = express.Router();
 
 router.get('/getOrdersHistory', orderController.getOrdersHistory);
+router.get('/getLastOrder', orderController.getLastOrder);
 
 export default router;
